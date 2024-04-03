d857893374110943ce831ee54c8e2c133d14d7e2
Sma3in96 <oumouss.ismail@gmail.com>
2024-04-03 02:18:10 +0000

    setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-red-500 p-4">this a page</div>
+    <div class="bg-red-500">
+        <router-view />
+    </div>
+</template>
+
+<script setup>
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Contacts from './views/Contacts.vue'
+import LogIn from './views/LogIn.vue'
+import SignUp from './views/SignUp.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/About',
+            component: About
+        },
+        {
+            path: '/Contacts',
+            component: Contacts
+        },
+        {
+            path: '/Login',
+            component: LogIn
+        },
+        {
+            path: '/Signup',
+            component: SignUp
+        }
+    ]
+})
+
+export default router
